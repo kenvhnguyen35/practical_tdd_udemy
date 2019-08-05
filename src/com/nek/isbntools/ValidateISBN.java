8af2467968882129487ddf78ff80250a406b3bee
hoangnguyen <hoangnguyen@blah>
2019-08-05 23:46:43 +0200

    test 13 digit ISBN

--- src/com/nek/isbntools/ValidateISBN.java
+++ src/com/nek/isbntools/ValidateISBN.java
@@ -2,25 +2,40 @@ package com.nek.isbntools;
 
 public class ValidateISBN {
     public boolean checkISBN(String isbn) {
-        if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits");
+        if (isbn.length() != 10 && isbn.length() != 13) throw new NumberFormatException("ISBN numbers must be 10 or 13 digits");
         //if (isbn.matches("^[a-zA-Z]*$")) throw new NumberFormatException("ISBN numbers must contains only digits");
         int total = 0;
-        for (int i = 0; i < 10; i ++) {
-            if (!Character.isDigit(isbn.charAt(i))) {
-                if (i == 9 && isbn.charAt(i) == 'X') {
-                    total += 10; // according to wikipedia
+        if (isbn.length() == 10) {
+            for (int i = 0; i < 10; i ++) {
+                if (!Character.isDigit(isbn.charAt(i))) {
+                    if (i == 9 && isbn.charAt(i) == 'X') {
+                        total += 10; // according to wikipedia
+                    }
+                    else {
+                        throw new NumberFormatException("ISBN numbers must contains only digits");
+                    }
+                } else {
+                    total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
                 }
-                else {
-                    throw new NumberFormatException("ISBN numbers must contains only digits");
-                }
-            } else {
-                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
             }
-        }
-        if (total % 11 == 0) {
-            return true;
+            if (total % 11 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
-            return false;
+            for (int i = 0; i < 13; i++) {
+                if (i % 2 == 1) {
+                    total += Character.getNumericValue(isbn.charAt(i)) * 3;
+                } else {
+                    total += Character.getNumericValue(isbn.charAt(i));
+                }
+            }
+            if (total % 10 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
--- test/com/nek/isbntools/ValidateISBNTest.java
+++ test/com/nek/isbntools/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class ValidateISBNTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkAValid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
         assertTrue("first value to test", result);
@@ -16,9 +16,27 @@ public class ValidateISBNTest {
     }
 
     @Test
-    public void checkAnInvalidISBN() {
+    public void checkAValid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781846558238");
+        assertTrue(result);
+        result = validator.checkISBN("9781853260087");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitISBN() {
         ValidateISBN validatorISBN = new ValidateISBN();
         boolean result = validatorISBN.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781846558232");
+        assertFalse(result);
+        result = validator.checkISBN("9781853260082");
         assertFalse(result);
     }
 
@@ -35,7 +53,7 @@ public class ValidateISBNTest {
     }
 
     @Test
-    public void IsbnNumbersEndingInAnXAreValid() {
+    public void TenDigitISBNNumbersEndingInAnXAreValid() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("012000030X");
         assertTrue(result);
