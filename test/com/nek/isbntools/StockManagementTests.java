6169e4b38a45b9824d0289506a87f8112dbe4655
hoangnguyen <hoangnguyen@blah>
2019-08-07 21:42:42 +0200

    using @Before to setup shared objects before all tests

--- test/com/nek/isbntools/StockManagementTests.java
+++ test/com/nek/isbntools/StockManagementTests.java
@@ -1,5 +1,6 @@
 package com.nek.isbntools;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,15 +8,24 @@ import static org.mockito.Mockito.*;
 
 
 public class StockManagementTests {
-    @Test
-    public void testCanGetACorrectLocatorCode() {
-        ExternalISBNDataService fakeWebService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService fakeDatabaseService = mock(ExternalISBNDataService.class);
 
-        String isbn = "0140177396";
-        StockManager stockManager = new StockManager();
+    ExternalISBNDataService fakeWebService;
+    ExternalISBNDataService fakeDatabaseService;
+    StockManager stockManager;
+    String isbn = "0140177396";
+
+    @Before
+    public void setup() {
+        System.out.println("setup running before every test, refresh setup objects...");
+        fakeWebService = mock(ExternalISBNDataService.class);
+        fakeDatabaseService = mock(ExternalISBNDataService.class);
+        stockManager = new StockManager();
         stockManager.setExternalWebService(fakeWebService);
         stockManager.setDatabaseService(fakeDatabaseService);
+    }
+
+    @Test
+    public void testCanGetACorrectLocatorCode() {
 
         when(fakeDatabaseService.lookUp(anyString())).thenReturn(null);
         when(fakeWebService.lookUp(isbn)).thenReturn(new Book(isbn, "Of Mice And Men", "J. Steinbeck"));
@@ -27,13 +37,6 @@ public class StockManagementTests {
 
     @Test
     public void databaseIsUsedIfDataIsPresent() {
-        ExternalISBNDataService fakeWebService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService fakeDatabaseService = mock(ExternalISBNDataService.class);
-
-        String isbn = "0140177396";
-        StockManager stockManager = new StockManager();
-        stockManager.setExternalWebService(fakeWebService);
-        stockManager.setDatabaseService(fakeDatabaseService);
 
         when(fakeDatabaseService.lookUp(isbn)).thenReturn(new Book(isbn, "blah", "bleh"));
 
@@ -45,13 +48,6 @@ public class StockManagementTests {
 
     @Test
     public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
-        ExternalISBNDataService fakeWebService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService fakeDatabaseService = mock(ExternalISBNDataService.class);
-
-        String isbn = "0140177396";
-        StockManager stockManager = new StockManager();
-        stockManager.setExternalWebService(fakeWebService);
-        stockManager.setDatabaseService(fakeDatabaseService);
 
         when(fakeDatabaseService.lookUp(anyString())).thenReturn(null); // this is a mock, but it is also a stub
         when(fakeWebService.lookUp(isbn)).thenReturn(new Book(isbn, "blah", "bleh")); // test only behavior, care not about logic!
